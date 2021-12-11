328eeb660d8a559695fcadef77424f090c380b74
annaliist <annaliis.tahevali@outlook.com>
2021-12-11 23:06:43 +0200

    tryna merge

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.static('Public'));
 
 app.listen(3000, () => {
- console.log("Server is listening to port 3000")
+    console.log("Server is listening to port 3000")
 });
 
 app.get('/about', (req, res) => {
@@ -26,76 +26,77 @@ app.get('/contact', (req, res) => {
     res.render('contact');
 });
 
-app.get('/', async(req, res) => {
- try {
- console.log("get posts request has arrived");
- const posts = await pool.query(
- "SELECT * FROM posts"
- );
- res.render('posts', { posts: posts.rows });
- } catch (err) {
- console.error(err.message);
- }
+app.get('/', async (req, res) => {
+    try {
+        console.log("get posts request has arrived");
+        const posts = await pool.query(
+            "SELECT * FROM posts"
+        );
+        res.render('posts', { posts: posts.rows });
+    } catch (err) {
+        console.error(err.message);
+    }
 });
 
-app.get('/singlepost/:id', async(req, res) => {
- try {
- const id = req.params.id;
- console.log(req.params.id);
- console.log("get a single post request has arrived");
- const posts = await pool.query(
- "SELECT * FROM posts WHERE id = $1", [id]
- );
- res.render('singleposts', { posts: posts.rows[0] });
- } catch (err) {
- console.error(err.message);
- }
+app.get('/singlepost/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        console.log(req.params.id);
+        console.log("get a single post request has arrived");
+        const posts = await pool.query(
+            "SELECT * FROM posts WHERE id = $1", [id]
+        );
+        res.render('singleposts', { posts: posts.rows[0] });
+    } catch (err) {
+        console.error(err.message);
+    }
 });
 
-app.get('/posts/:id', async(req, res) => {
- try {
- const { id } = req.params;
- console.log("get a post request has arrived");
- const Apost = await pool.query(
- "SELECT * FROM posts WHERE id = $1", [id]
- );
- res.json(Apost.rows[0]);
- } catch (err) { console.error(err.message);
- }
+app.get('/posts/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log("get a post request has arrived");
+        const Apost = await pool.query(
+            "SELECT * FROM posts WHERE id = $1", [id]
+        );
+        res.json(Apost.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
 });
 
-app.delete('/posts/:id', async(req, res) => {
- try {
- console.log(req.params);
- const { id } = req.params;
- const post = req.body;
- console.log("delete a post request has arrived");
- const deletepost = await pool.query(
- "DELETE FROM posts WHERE id = $1", [id]
- );
- res.redirect('posts');
- } catch (err) {
- console.error(err.message);
- }
+app.delete('/posts/:id', async (req, res) => {
+    try {
+        console.log(req.params);
+        const { id } = req.params;
+        const post = req.body;
+        console.log("delete a post request has arrived");
+        const deletepost = await pool.query(
+            "DELETE FROM posts WHERE id = $1", [id]
+        );
+        res.redirect('posts');
+    } catch (err) {
+        console.error(err.message);
+    }
 });
 
-app.post('/create', async(req, res) => {
- try {
- const post = req.body;
- console.log(post);
- const newpost = await pool.query(
- "INSERT INTO posts(author, body, date, image, authorImage) values ($1, $2, $3) RETURNING*", [post.author, post.body, post.date, post.image, post.authorImage]
- );
- res.redirect('/');
- } catch (err) {
- console.error(err.message)
- }
+app.post('/posts', async (req, res) => {
+    try {
+        const post = req.body;
+        console.log(post);
+        const newpost = await pool.query(
+            "INSERT INTO posts(author, body, date, image, authorImage) values ($1, $2, $3) RETURNING*", [post.author, post.body, post.date, post.image, post.authorImage]
+        );
+        res.redirect('/');
+    } catch (err) {
+        console.error(err.message)
+    }
 });
 
 app.get('/create', (req, res) => {
- res.render('create');
+    res.render('create');
 });
 
 app.use((req, res) => {
- res.status(404).render('404');
+    res.status(404).render('404');
 });
